bc54c488f07b62f7df7e08b107f5dc44a9d5edc7
VINCENTIUS CHRISTIAN AJI WIANTO <vincentius.wianto@binus.ac.id>
2020-12-09 00:18:37 +0700

    wow

--- app/src/main/java/com/example/moneymanager/SplashScreen.java
+++ app/src/main/java/com/example/moneymanager/SplashScreen.java
@@ -1,8 +1,8 @@
 package com.example.moneymanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +10,6 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        //WOW
     }
 }
