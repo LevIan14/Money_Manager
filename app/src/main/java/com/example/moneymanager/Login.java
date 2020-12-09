42cc0564b31f57ad9e8c111219746e1f40444b9e
Levi Ian <bgi.c57@gmail.com>
2020-12-09 20:31:52 +0700

    Fixing Login Bug

--- app/src/main/java/com/example/moneymanager/Login.java
+++
@@ -1,5 +0,0 @@
-package com.example.moneymanager;
-
-public class Login {
-
-}
---
+++ app/src/main/java/com/example/moneymanager/StartMenu/Login.java
@@ -0,0 +1,15 @@
+package com.example.moneymanager.StartMenu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.moneymanager.R;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
