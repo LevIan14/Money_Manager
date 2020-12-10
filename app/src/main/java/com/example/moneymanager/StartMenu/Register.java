c202b9d34f9251bce0ce10044b1990936db2e04e
Astrid <astridnovitaan@gmail.com>
2020-12-10 12:18:12 +0700

    activity_register.xml Register.java ic_baseline input_bg

---
+++ app/src/main/java/com/example/moneymanager/StartMenu/Register.java
@@ -0,0 +1,15 @@
+package com.example.moneymanager.StartMenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.moneymanager.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
